1675db07ecbc54359bc45b347e9420ec8828c1a2
yoshi2shi <yoshi2shi@gmail.com>
2014-08-15 05:20:54 -0400

    Delete rotate.js

--- rotate.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by hailemichaelteklai on 8/15/14.
- */
-var sheet = (function(){
-    var style = document.createElement('sytle');
-    style.appendChild(document.createTextNode(" "));
-    document.head.appendChild(style);
-    return style.sheet;
-})();
